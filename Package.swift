9e8025e53de7110dbef56e90011f55b5c06f8e0b
Sascha Müllner <sascha.muellner@gmail.com>
2026-09-24 17:52:03 +0200

    Prepare ISO639 1.5.0

--- Package.swift
+++ Package.swift
@@ -6,10 +6,11 @@ import PackageDescription
 let package = Package(
     name: "ISO639",
     defaultLocalization: "en",
-    platforms: [ .iOS(.v13),
-                 .macOS(.v10_15),
-                 .tvOS(.v13),
-                 .watchOS(.v6)
+    platforms: [
+        .iOS(.v14),
+        .macOS(.v14),
+        .tvOS(.v14),
+        .watchOS(.v6)
     ],
     products: [
         .library(
